d0d94e15fd2be9e142b33cb1f5128ce830f4b374
mp4393939 <2822434022@qq.com>
2019-07-14 20:41:48 +0800

    add

---
+++ src/Array/ReadArray.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class ReadArray extends order {
+    public static void main(String[] args) {
+        ReadArray read = new ReadArray();
+        a();
+        System.out.println("\n");
+        read.b();
+    }
+}
