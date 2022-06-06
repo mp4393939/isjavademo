798ec14f11956bd59cc23945f6f4810993478b32
mp4393939 <2822434022@qq.com>
2022-06-06 19:42:27 +0800

    add new class and modified class

--- out/production/isjavademo/src/io/OutputDiskLoop.java
+++ out/production/isjavademo/src/io/OutputDiskLoop.java
@@ -1,6 +1,5 @@
 package io;
 
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -8,7 +7,7 @@ import java.io.PrintStream;
 public class OutputDiskLoop {
     public static void main(String[] args) throws IOException {
         FileOutputStream file;
-        PrintStream ps = null;
+        PrintStream ps;
         file = new FileOutputStream("FileOutputStream.docx", false);//如果第二个参数为true，则字节将写入文件的末尾而不是开头;false会清空原来的文本
         ps = new PrintStream(file);
         StringBuilder sb=new StringBuilder();
